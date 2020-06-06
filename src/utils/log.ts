9b5faa74b95b0d0810a1854f67b871a69e6cde7f
Élise <atomheartother@gmail.com>
2020-06-06 18:18:47 +0200

    Changed log format

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -6,12 +6,12 @@ import { getInfo, SupportedChannel } from '../channel';
 const log = (message : any, channel : SupportedChannel = null) : void => {
   const dateString = new Date().toLocaleString('en-GB');
   if (!channel) {
-    console.log(dateString);
+    console.log(`[${dateString}]`);
     console.log(message);
     return;
   }
   const channelInfo = getInfo(channel);
-  console.log(`${dateString}\t${channelInfo}`);
+  console.log(`[${dateString}\t${channelInfo}]`);
   console.log(message);
 };
 
