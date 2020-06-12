733782e851d36df08a0eb0f87599635afb54b87e
Élise <atomheartother@gmail.com>
2020-06-12 21:04:34 +0200

    Create character if we don't know them

--- src/utils/getters.ts
+++ src/utils/getters.ts
@@ -1,11 +1,17 @@
 import { Guild } from 'discord.js';
 import { Character } from '../db/characters';
-import { getCharacter } from '../db';
+import { getCharacter, createCharacter } from '../db';
 import { getRoleFromMention } from '../discord';
 
+// eslint-disable-next-line import/prefer-default-export
 export const getCharFromStr = async (charStr: string, guild: Guild) : Promise<Character> => {
   const role = getRoleFromMention(guild, charStr);
   if (role) {
+    // We have a role. Check if there's a character assigned to it
+    // And if not, create it.
+    const char = await getCharacter(role.id);
+    if (char) return char;
+    await createCharacter(role.id);
     return getCharacter(role.id);
   }
   return null;
