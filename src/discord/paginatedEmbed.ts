408de48d2a81e798ea2105970098175a0f15c884
Liz <atomheartother@gmail.com>
2020-11-19 11:15:26 +0100

    Don't do any of the pagination stuff it there's only one page

--- src/discord/paginatedEmbed.ts
+++ src/discord/paginatedEmbed.ts
@@ -11,43 +11,45 @@ export const paginatedEmbedWithFormat = async <T = MessageEmbed>(
 ): Promise<Message> => {
   let page = 0;
   const sentEmbed = await channel.send({ embed: (await formatFunc(pages[page])).setFooter(`Page ${page + 1} / ${pages.length}`) });
-  emojiList.forEach((emoji) => {
-    sentEmbed.react(emoji);
-  });
-  const reactionCollector = sentEmbed.createReactionCollector(
-    (reaction, user) => emojiList.includes(reaction.emoji.name) && !user.bot,
-    { time },
-  );
+  if (pages.length > 1) {
+    emojiList.forEach((emoji) => {
+      sentEmbed.react(emoji);
+    });
+    const reactionCollector = sentEmbed.createReactionCollector(
+      (reaction, user) => emojiList.includes(reaction.emoji.name) && !user.bot,
+      { time },
+    );
 
-  const editEmbed = async (newPage: number): Promise<void> => {
-    if (newPage === page) return;
-    try {
-      const newEmbed = await formatFunc(pages[newPage]);
-      sentEmbed.edit({ embed: newEmbed.setFooter(`Page ${newPage + 1} / ${pages.length}`) });
-      page = newPage;
-    } catch (e) {
-      console.error('paginatedEmbed: Failed editing embed:', e);
-    }
-  };
+    const editEmbed = async (newPage: number): Promise<void> => {
+      if (newPage === page) return;
+      try {
+        const newEmbed = await formatFunc(pages[newPage]);
+        sentEmbed.edit({ embed: newEmbed.setFooter(`Page ${newPage + 1} / ${pages.length}`) });
+        page = newPage;
+      } catch (e) {
+        console.error('paginatedEmbed: Failed editing embed:', e);
+      }
+    };
 
-  reactionCollector.on('collect', async (reaction, user) => {
-    reaction.users.remove(user);
-    switch (reaction.emoji.name) {
-      case emojiList[0]:
-        editEmbed(page > 0 ? page - 1 : pages.length - 1);
-        break;
-      case emojiList[1]:
-        editEmbed(page + 1 < pages.length ? page + 1 : 0);
-        break;
-      default:
-        break;
-    }
-  });
-  reactionCollector.on('end', () => {
-    if (!sentEmbed.deleted) {
-      sentEmbed.reactions.removeAll();
-    }
-  });
+    reactionCollector.on('collect', async (reaction, user) => {
+      reaction.users.remove(user);
+      switch (reaction.emoji.name) {
+        case emojiList[0]:
+          editEmbed(page > 0 ? page - 1 : pages.length - 1);
+          break;
+        case emojiList[1]:
+          editEmbed(page + 1 < pages.length ? page + 1 : 0);
+          break;
+        default:
+          break;
+      }
+    });
+    reactionCollector.on('end', () => {
+      if (!sentEmbed.deleted) {
+        sentEmbed.reactions.removeAll();
+      }
+    });
+  }
   return sentEmbed;
 };
 
